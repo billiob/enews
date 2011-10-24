ca7cd5ba375b02ae45f30e1b16606e70d0dec940
Boris 'billiob' Faure <billiob@gmail.com>
2011-10-24 21:49:06 +0200

    was missing <iframe> from html tags

--- src/utils.c
+++ src/utils.c
@@ -63,13 +63,13 @@ char *extract_text_from_html(const char *src)
                 "body", "br", "button", "caption", "cite", "code", "col",
                 "colgroup", "dd", "del", "dfn", "div", "dl", "DOCTYPE", "dt",
                 "em", "fieldset", "form", "h1", "h2", "h3", "h4", "h5", "h6",
-                "head", "html", "hr", "i", "img", "input", "ins", "kbd",
-                "label", "legend", "li", "link", "map", "meta", "noscript",
-                "object", "ol", "optgroup", "option", "p", "param", "pre",
-                "q", "samp", "script", "select", "small", "span", "strong",
-                "style", "sub", "sup", "table", "tbody", "td", "textarea",
-                "tfoot", "th", "thead", "title", "tr", "tt", "ul", "var",
-                "!--", NULL};
+                "head", "html", "hr", "i", "iframe", "img", "input", "ins",
+                "kbd", "label", "legend", "li", "link", "map", "meta",
+                "noscript", "object", "ol", "optgroup", "option", "p",
+                "param", "pre", "q", "samp", "script", "select", "small",
+                "span", "strong", "style", "sub", "sup", "table", "tbody",
+                "td", "textarea", "tfoot", "th", "thead", "title", "tr", "tt",
+                "ul", "var", "!--", NULL};
 
             eina_strbuf_append_length(sb, old_pos, src - old_pos);
             src = _skip_spaces(++src);
